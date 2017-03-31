6eebb05a026d1a719e28738976b849271a5c6211
Andriy Byalyk <rewrite.andriy@gmail.com>
2017-03-31 09:26:43 +0300

    Tests added

---
+++ spec/models/product_specs.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+# rake db:seed RAILS_ENV=test
+RSpec.describe Product, :type => :model do
+  it "has valid output with one argument" do
+    expect(Product.with_categories('Fiction').pluck(:id).sort)
+      .to match_array([1, 3, 4, 5, 6])
+  end
+
+  it "has zero output with missing element" do
+    expect(Product.with_categories('Science').pluck(:id).count).to eq(0)
+  end
+
+  it "has valid output for array of categories" do
+    expect(Product.with_categories(['Fiction', 'Science Fiction'])
+      .pluck(:id).count).to eq(2)
+  end
+
+  it "has zero output for duplicated elements" do
+    expect(Product.with_categories(['Science','Science']).pluck(:id).count)
+      .to eq(0)
+  end
+
+end
